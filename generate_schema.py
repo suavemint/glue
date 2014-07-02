e096c1d88e1cc2792f0878752b537acd50f35bf4
James Orcutt <ej.orcutt@gmail.com>
2014-07-02 11:11:24 -0400

    Works, albeit slowly. Fixed stupid damn correct handling of file objects; apparently do NOT close them....

--- generate_schema.py
+++ generate_schema.py
@@ -2,9 +2,76 @@ import psycopg2 as psql, sys, csv, logging, time
 import datetime
 
 ## NB: For large files (>131,074 b? B?), set csv.field_size_limit(N) for N > 131074
+def generate_schema(i):
 
-#pass
-heuristics = (lambda value: datetime.strptime(value, '%Y-%m-%d'), int, float)
+    import csv, logging, time
+
+    schema_start_time = time.time()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    header_max_values, header_types, table_data = [], [], []
+    d = ''
+
+    print 'Input file is',i
+
+    if i.lower().endswith('.tab'):
+        d = '\t'
+    if i.lower().endswith('.csv'):
+        d = ','
+        
+#    in_file = open(i,'rb')
+    csv_file = csv.reader(open(i,'rb'), delimiter=d, quoting=csv.QUOTE_NONE)    
+    header_labels = csv_file.next()#.split(',')
+#    header_labels = header_labels.split(',')
+#    print header_labels[:-1]
+#    print 15*'<'
+#    print header_labels[:-1]
+#    print 15*'<'
+    trimmed_end = header_labels[-1].replace('\r\n','')
+    header_labels[-1] = trimmed_end
+    #print header_labels
+	#file_in = csv.reader(in_file, delimiter=d, quoting=csv.QUOTE_NONE)
+    #header_labels = input_file.next()
+    #print header_labels
+#    print header_labels    
+#    print 'There are %i columns in file %s.' % (len(header_labels), f_name)
+#    print d
+    header_string = ','.join([x for x in header_labels])
+
+    # Clean the column titles.
+    for entry in header_labels:
+        index = header_labels.index(entry)
+        header_max_values.append(len(entry))
+        # PostgreSQL does not like "@" as an initial char in a column name, as that's the cursor.
+        if '@' in entry:
+            header_labels[index] = entry.replace('@','at')
+
+    # Loop over rows in csv.
+    for row in csv_file:
+#        print row
+        for entry in row:
+            length = len(entry)
+            if length > header_max_values[index]:
+                header_max_values[index] = length
+            else:
+                continue
+#	in_file.close()
+
+	# Generate the outputted string, which will contain the variable type and char-limit for each column.
+    string_to_add = ''
+    
+    for x, num in zip(header_labels, header_max_values):
+        string_to_add += '%s varchar(%s), ' % (x, num)
+    schema_end_time = time.time()
+    print 'Schema generation took %.2f seconds.' % (schema_start_time - schema_end_time)
+
+    return header_string, string_to_add
+## end generate_schema
+
+
+heuristics = (lambda value: datetime.strptime(value, '%m-%d-%Y'), int, float)
 
 def check_and_set_type(n):
 #    pass
@@ -16,115 +83,111 @@ def check_and_set_type(n):
         # If input fails all heuristics, input is defaulted to string
     return n
 
+if __name__ == '__main__':
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-logger.debug('Running script...')
-logger.debug('Begin opening CSV/TAB file.')
+	logging.basicConfig(level=logging.DEBUG)
+	logger = logging.getLogger(__name__)
+	logger.debug('Running script...')
+	logger.debug('Begin opening CSV/TAB file.')
 
-start_time = time.time()
 
-# First, let's take in a csv file (or a tsv file):
-d = ''
-f_name = 'FL GOP 050914.csv'
-f_stub = f_name.split('.')[0]
-#f_name = 'VoterMapping--AR--04-11-2014-HEADERS.tab'
+	d = ''
+	f_name = 'FL GOP 050914.csv'
+	#f_name = 'clipped.csv'
+	#f_stub = f_name.split('.')[0].replace(' ','_')
+	#f_name = 'VoterMapping--AR--04-11-2014-HEADERS.tab'
 
-if f_name.lower().endswith('.tab'):
-    d = '\t'
-if f_name.lower().endswith('.csv'):
-    d = ','
-    
-input_file = csv.reader(open(f_name,'rb'), delimiter=d, quoting=csv.QUOTE_NONE)
-header_labels = input_file.next()
+	# Open the input file once, and output the schema.
+	schema_string, header_labels = generate_schema(f_name)    
 
-print 'There are %i headers in this file, %s.' % (len(header_labels), f_name)
-header_max_values = []
-header_types = []
-table_data = []
+	schema_name = 'test_schema'
+	table_name = 'test_table'
+	sample_schema_cmd = 'CREATE %s' % schema_name
 
-#header_header_string = ','.join([x for x in header_labels])
+		
+	# TODO: consider NOT NULL cases, etc. Is this constraint applicable to our case(s)?
+	#sample_schema = 'CREATE TABLE %s.%s (%s);' % (schema_name, table_name, string_short)
+	sample_schema = 'CREATE TABLE %s (%s);' % (table_name, schema_string[:-2])
 
-for entry in header_labels:
-    index = header_labels.index(entry)
-    header_max_values.append(len(entry))
-    if '@' in entry:
-	header_labels[index] = entry.replace('@','at')
-    if entry[:4].isdigit():
-        entry = '_' + entry
-# We'll start by assuming all inputted vartypes are string, and build from there.
-for row in input_file:
-    out = ','.join([x for x in row])
-#    table_data.append((row))
-#    print row, type(row)
-    for entry in row:
-        index = row.index(entry)
-#        if '@' in entry:
-#            print 'PROBLEM FOUND WITH INPUT STRING:', entry, 'in row', row
-#            row[entry] = entry.replace('@','at')
+	# Now that we have the entire SQL query ready to go, let's dump it in a PSQL DB:
+	connection = None
 
-        length = len(entry)
-        if length > header_max_values[index]:
-            header_max_values[index] = length
-        else:
-            continue
+	logger.info('Starting DB ops.')
 
-finish_time = time.time()
-#print header_max_values
-#input_file.close()
-logger.debug('CSV/TAB file closed.')
-logger.debug('Scanning completed.')
+	try:
+		connection = psql.connect(database='testdb',user='postgres',password='password')
+		cursor = connection.cursor()
 
-print 'Scanning took %i seconds' % (finish_time - start_time)
-# 'create schema %s' % schema_name  # Now the table + schema are acessed with 'schema_name.table_name' (or, in SQL, 'database_name.schema_name.table_name').
-#
-schema_name = 'test_schema'
-table_name = 'test_table'
-sample_schema_cmd = 'CREATE %s' % schema_name
+		num_args = len(row)
+		
+		cursor.execute('DROP TABLE IF EXISTS %s' % table_name)
+		cursor.execute(sample_schema)
 
-string_to_add = ''
-for x, num in zip(header_labels, header_max_values):
-#    print x,num
-    string_to_add += '%s varchar(%s), ' % (x, num)
+		command_str = 'INSERT INTO %s VALUES' % table_name
 
-# FIXME: Stop this string-copy nonsense (strs are immutable in py)
-# + in removing the terminal comma.
-#print 'Query test: %s' % string_to_add
-#logger.debug('%s' % string_to_add)
-string_short = string_to_add[:-2]
-    
-# TODO: consider NOT NULL cases, etc. Is this constraint applicable to our case(s)?
-#sample_schema = 'CREATE TABLE %s.%s (%s);' % (schema_name, table_name, string_short)
-sample_schema = 'CREATE TABLE %s (%s);' % (table_name, string_short)
-#print sample_schema
+		#header = ','.join([x for x in header_labels])
 
-# Now that we have the entire SQL query ready to go, let's dump it in a PSQL DB:
-connection = None
+		db_start_time = time.time()
 
-try:
-    connection = psql.connect(database='testdb',user='james')
-    logger.info(connection.autocommit)
+		# Open the input file again, skipping header row, and write-in each row.
+		with open(f_name,'rb') as f:
+			input_file = csv.reader(f, delimiter=d, quoting=csv.QUOTE_NONE)
+			input_file.next()
+			for row in input_file:
+				i = row.index(entry)
+				for entry in row:
+					row[i] = psql.extensions.adapt(entry).getquoted()
+				values = ','.join([z for z in row])
+				try:
+					cursor.execute( 'INSERT INTO %s(%s) VALUES (%s)' % (table_name, header, values) )
+				except Exception, e:
+					print 'Error!', e
+					print header, '\n\n', row    
+		#for row in table_data:
+	#        print row
+	#        print 'Length of row items:', len(row)
+	#        for entry in row:
+	#   	    print entry
+				#if entry == '' or entry == ' ':
+				#    row[i] = None    
+	##            if ' ' in entry and entry is not ' ':
+	##                row[i] = psql.extensions.adapt(entry).getquoted()
+			#row[i] = '\"'+entry+'\"'
+			#print str(entry)
+				#row[i] = psql.extensions.adapt(entry).getquoted()  # FIXME: May not need to adapt() every single GD entry...
 
-    cursor = connection.cursor()
+	##        row = tuple(row)
+	##	tuple_string = '"('+ num_args*'%s, '
+	##	tuple_string = tuple_string[:-1] + ')"'
 
-    # For now, we're calling the test table "dump_test"...
-    cursor.execute('DROP TABLE IF EXISTS dump_test')
-    cursor.execute(sample_schema)
-    for d in table_data:
-        print d
-        cursor.execute( 'INSERT INTO dump_test(%s) VALUES (%s)' % (','.join([x for x in header_labels]), ','.join([x for x in d])) )
+		
+	#	    sys.exit(1)
+		#print num_args
+		#print tuple_string
 
-    connection.commit()
-    
-except psql.DatabaseError, e:
-    if connection:
-        connection.rollback()  # Fail gracefully, and don't step on table with new data.
-    logger.warn('WARNING: Database failed: %s' % e)
-    print 15*'='
-    print ','.join([x for x in header_labels])
-    print 15*'='
-    #print table_data
-    sys.exit(1)
-finally:
-    if connection:
-        connection.close()  # Regardless of outcome, close connection safely.
+	# These following two numbers should be and are 64, so they match up in length.    
+	#    print len(tuple_string.split(', '))
+	#    print len(row)#print len(row)
+
+		#sys.exit(1)
+
+	##    query = ','.join( cursor.mogrify(tuple_string, x) for x in row )
+		#print query
+			#cursor.execute( 'INSERT INTO %s (%s) VALUES (%s)' % (table_name, ','.join([x for x in header_labels]), ','.join([x for x in row])) )
+		# FIXME: May not need to specify the column names for each execution... try it without the first list.  <-- NOT TRUE!
+		#cursor.execute( command_str + query )
+	#    query = 'INSERT INTO test_table 
+	#    cursor.executemany(
+		connection.commit()
+		db_end_time = time.time()
+		logger.info('DB write ops took %.2f seconds.' % (db_end_time - db_start_time))
+		
+	except psql.DatabaseError, e:
+		if connection:
+			connection.rollback()  # Fail gracefully, and don't step on table with new data.
+		logger.warn('WARNING: Database failed: %s' % e)
+	finally:
+		if connection:
+			connection.close()  # Regardless of outcome, close connection safely.
+
+
